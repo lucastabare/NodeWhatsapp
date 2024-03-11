3d0683e869a359b4c57e564f2bf4a11ced3cb0a4
Lucas Tabare <lucas.tabare@gmail.com>
2024-03-10 23:35:32 -0300

    Apis para eliminar sesiones

--- src/Controllers/MessageController.js
+++ src/Controllers/MessageController.js
@@ -3,88 +3,6 @@ const { ObtenerMensajes, ActualizarMensajes } = require('../Services/MessageServ
 const { ObtenerSesionWhatsapp } = require('../Services/SessionService');
 const path = require('path');
 
-// const enviarMensajes = async (req, res) => {
-//     const dataPath = path.resolve(__dirname, '../Sesiones');
-//     const idTelefono = req.params.idTelefono;
-
-//     try {
-//         const sessionState = ObtenerSesionWhatsapp(idTelefono);
-
-//         console.log("soy la session =>", sessionState)
-
-//         if (!sessionState) {
-//             return res.status(400)
-//                 .send('El teléfono no está conectado');
-//         }
-
-//         const whatsappClient = new Client({
-//             authStrategy: new LocalAuth({
-//                 clientId: `cliente-${idTelefono}`,
-//                 dataPath: dataPath,
-//                 webVersion: '2.2306.7',
-//             }),
-//             puppeteer: { headless: true },
-//         });
-
-//         // whatsappClient.on('ready', async () => {
-//         //     console.log('Cliente de WhatsApp listo y conectado');
-
-//         //     for (let message of messages.slice(0, 100)) {
-//         //         const { numero, texto, id } = message;
-//         //         let numeroLimpio = message.numero.charAt(0) === '+' ? '+' + message.numero.slice(1).replace(/[^0-9]/g, '') : message.numero.replace(/[^0-9]/g, '');
-//         //         // Asegúrate de que el número esté en formato internacional completo y añade '@c.us'
-//         //         let numeroFormateado = numeroLimpio.includes('@c.us') ? numeroLimpio : `${numeroLimpio}@c.us`;
-
-//         //         try {
-//         //             await whatsappClient.sendMessage(numeroFormateado, texto);
-//         //             console.log(`Mensaje enviado a ${numero}`);
-
-//         //             await ActualizarMensajes(id, 4);
-
-//         //             // Introduce un retraso de 15 segundos antes de enviar el próximo mensaje
-//         //             await new Promise(resolve => setTimeout(resolve, 15000));
-
-//         //         } catch (err) {
-//         //             console.error(`Error al enviar mensaje a ${numero}: ${err}`);
-//         //         }
-//         //     }
-
-//         //     res.send('Mensajes enviados');
-//         // });
-
-//         whatsappClient.initialize().then(async () => {
-//             whatsappClient.on('ready', async () => {
-//                 console.log('Cliente de WhatsApp listo y conectado');
-//                 const messages = await ObtenerMensajes(idTelefono);
-//                 console.log("soy los mensajes ==>", messages);
-
-//                 for (let message of messages) {
-//                     const { numero, texto, id } = message;
-//                     let numeroFormateado = numero.includes('@c.us') ? numero : `${numero}@c.us`;
-
-//                     try {
-//                         await whatsappClient.sendMessage(numeroFormateado, texto);
-//                         console.log(`Mensaje enviado a ${numero}`);
-//                         await ActualizarMensajes(id, 4);
-
-//                     } catch (err) {
-//                         console.error(`Error al enviar mensaje a ${numero}: ${err}`);
-//                     }
-//                 }
-
-//                 res.send('Mensajes enviados');
-//             });
-//         }).catch(err => {
-//             console.error('Error al inicializar la sesión de WhatsApp', err);
-//             res.status(500).send('Error al inicializar la sesión de WhatsApp');
-//         });
-
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Error al enviar mensajes');
-//     }
-// };
-
 const enviarMensajes = async (req, res) => {
     const dataPath = path.resolve(__dirname, '../Sesiones');
     const idTelefono = req.params.idTelefono;
@@ -109,24 +27,16 @@ const enviarMensajes = async (req, res) => {
             const messages = await ObtenerMensajes(idTelefono);
             console.log("soy los mensajes ==>", messages);
 
-            // for (let message of messages) {
-            //     const { numero, texto, id } = message;
-            //     let numeroFormateado = numero.includes('@c.us') ? numero : `${numero}@c.us`;
-
-            //     try {
-            //         await whatsappClient.sendMessage(numeroFormateado, texto);
-            //         console.log(`Mensaje enviado a ${numero}`);
-            //         await ActualizarMensajes(id, 4);
-            //     } catch (err) {
-            //         console.error(`Error al enviar mensaje a ${numero}: ${err}`);
-            //     }
-            // }
+            if (messages.length === 0) {
+                whatsappClient.destroy();
+                return res.status(200)
+                    .send('No hay mensajes para enviar');
+            }
 
             for (let message of messages) {
                 const { numero, texto, id } = message;
 
-                // Asegúrate de que el número está en el formato correcto
-                let numeroLimpio = numero.replace(/[^\d]/g, "");  // Elimina todo lo que no sea dígitos
+                let numeroLimpio = numero.replace(/[^\d]/g, "");
                 if (!numeroLimpio.startsWith("549")) {  // Asegúrate de que comienza con el código de país sin '+'
                     numeroLimpio = "549" + numeroLimpio;  // Añade el código de país si no está presente
                 }
@@ -141,7 +51,10 @@ const enviarMensajes = async (req, res) => {
                 }
             }
 
-            res.send('Mensajes enviados');
+            whatsappClient.destroy();
+            res.status(200)
+                .send('Mensajes enviados');
+
         } catch (error) {
             console.error('Error al enviar mensajes:', error);
             res.status(500).send('Error al enviar mensajes');
--- src/Routes/WhatsappRoutes.js
+++ src/Routes/WhatsappRoutes.js
@@ -2,25 +2,9 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../Controllers/MessageController'); // Verifica la ruta al controlador de mensajes
 const qrController = require('../Controllers/QrController'); // Verifica la ruta al controlador de QR
+const sesionController = require('../Controllers/SessionContoller')
 
-/**
- * @swagger
- * /api/whatsapp/EnviarMensajes/{idTelefono}:
- *   post:
- *     summary: Envia mensajes de WhatsApp.
- *     parameters:
- *       - in: path
- *         name: idTelefono
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Mensaje enviado con éxito.
- */
-
-router.post('/EnviarMensajes/:idTelefono', messageController.enviarMensajes);
-
+//API PARA GENERAR QR 
 /**
  * @swagger
  * /api/whatsapp/GenerarQR/{idTelefono}:
@@ -39,5 +23,62 @@ router.post('/EnviarMensajes/:idTelefono', messageController.enviarMensajes);
 
 router.get('/GenerarQR/:idTelefono', qrController.generarQR);
 
-// Exportar el router para usarlo en app.js
+//API PARA ENVIAR MENSAJES
+/**
+ * @swagger
+ * /api/whatsapp/EnviarMensajes/{idTelefono}:
+ *   post:
+ *     summary: Envia mensajes de WhatsApp.
+ *     parameters:
+ *       - in: path
+ *         name: idTelefono
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Mensaje enviado con éxito.
+ */
+
+router.post('/EnviarMensajes/:idTelefono', messageController.enviarMensajes);
+
+//API PARA ELIMINAR SAESION SEGUN TELEFONO
+/**
+ * @swagger
+ * /api/whatsapp/EliminarSesion/{idTelefono}:
+ *   delete:
+ *     summary: Elimina la sesion guardada por id telefono.
+ *     parameters:
+ *       - in: path
+ *         name: idTelefono
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Sesion eliminada con exito.
+ */
+
+router.delete('/EliminarSesion/:idTelefono', sesionController.eliminarSession);
+
+//API PARA ELIMNAR TODAS LAS SESIONES GUARDADAS
+/**
+ * @swagger
+ * /api/whatsapp/EliminarSesion/{idTelefono}:
+ *   delete:
+ *     summary: Elimina todas las sesiones guardadas.
+ *     parameters:
+ *       - in: path
+ *         name: idTelefono
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Sesiones eliminadas con exito.
+ */
+
+router.delete('/EliminarSessiones/', sesionController.eliminarSessiones);
+
+
 module.exports = router;
